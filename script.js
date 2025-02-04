333eeedbe4df7a8a4c29ddc64ec8f8a56e6abfd2
Iasmym <iasmympatricia7@gmail.com>
2025-02-04 19:42:29 -0300

    vetores

---
+++ script.js
@@ -0,0 +1,28 @@
+// variaveis=  lugar que armazena valor variavel
+var nome = "iasmym" // tipo string ""
+
+var idade = 24 // tipo number
+
+var almocou = true // tipo boolean (v ou f)
+
+//alterando valor de variavel
+nome = "joao"
+idade = 18;
+almocou = false 
+
+//vetores= armazena um conjunto de elementos do mesmo tipo
+// vetores = lista de valores
+// vetores = []
+
+var listaNomes = ["ana clara", "ana vitoria",];
+var listaIdade = [23,25,29];
+
+//senhas -> index -> indice -> posiçao
+// index
+
+var listaNome = ["beatriz", "tabs" ]
+// substituir valor
+listaNome[0] = "joao"
+console.log(listaNome)
+
+
