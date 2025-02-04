50a4ef3a8877e1d088918a061488803659fee8c7
Iasmym <iasmympatricia7@gmail.com>
2025-02-04 20:22:11 -0300

    trabalho

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
-// variaveis=  lugar que armazena valor variavel
-var nome = "iasmym" // tipo string ""
+// // variaveis=  lugar que armazena valor variavel
+// var nome = "iasmym" // tipo string ""
 
-var idade = 24 // tipo number
+// var idade = 24 // tipo number
 
-var almocou = true // tipo boolean (v ou f)
+// var almocou = true // tipo boolean (v ou f)
 
-//alterando valor de variavel
-nome = "joao"
-idade = 18;
-almocou = false 
+// //alterando valor de variavel
+// nome = "joao"
+// idade = 18;
+// almocou = false 
 
-//vetores= armazena um conjunto de elementos do mesmo tipo
-// vetores = lista de valores
-// vetores = []
+// //vetores= armazena um conjunto de elementos do mesmo tipo
+// // vetores = lista de valores
+// // vetores = []
 
-var listaNomes = ["ana clara", "ana vitoria",];
-var listaIdade = [23,25,29];
+// var listaNomes = ["ana clara", "ana vitoria",];
+// var listaIdade = [23,25,29];
 
-//senhas -> index -> indice -> posiçao
-// index
+// //senhas -> index -> indice -> posiçao
+// // index
 
-var listaNome = ["beatriz", "tabs" ]
+var listaNome = ["iasmym", "camila" ]
 // substituir valor
-listaNome[0] = "joao"
-console.log(listaNome)
+// listaNome[0] = "joao"
+// console.log(listaNome)
 
 //nomeArray.push(item) - adiciona um item ao final do array
-listaNome.push("marcilene")
+listaNome.push("luna")
 console.log(listaNome)
+
 //nomearray.pop - remove o ultimo item do array
-listaNome.pop();
+listaNome.pop(simon);
+// ["beatrz", "nome2", "nome tal"]
 console.log(listaNome)
+
 //unshift - adiciona item no inicio do array
 listaNome.unshift("lauana");
 console.log(listaNome)
+
 //shift - remove o primeiro elemento do array e retorna o elemento
-listaNome.shift();
-console.log(listaNome)
+// listaNome.shift();
+// console.log(listaNome)
+
+// var listaMusicaFesta= []
+// var nomeMusica = prompt("digite uma musica")
+// var nomeMusica2 = prompt("digite uma musica" )
+// listaMusicaFesta.push(nomeMusica, nomeMusica2)
+// console.log(listaMusicaFesta);
+
+// documento.write(`<h1>${listaMusicaFesta[2]}</h1>`);
 
 
 
